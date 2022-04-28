7d9bba486dae02e9968091aa920c56734e09a611
Julian van Husen <210549@student.glu.nl>
2022-04-28 10:04:46 +0200

    if else localhost/u210549

--- db_conn.php
+++ db_conn.php
@@ -1,10 +1,20 @@
 <?php
 
-$sname= "localhost";
-$unmae= "root";
-$password = "";
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
 
-$db_name = "projectweek_p4";
+	$sname = "localhost";
+	$unmae = "root";
+	$password = "";
+
+	$db_name = "projectweek_p4";
+} else {
+	$sname = "localhost";
+	$unmae = "root";
+	$password = "";
+
+	$db_name = "projectweek_p4";
+}
+
 
 $conn = mysqli_connect($sname, $unmae, $password, $db_name);
 
